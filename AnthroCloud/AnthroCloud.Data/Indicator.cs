193e45a3a1c252edb7c5c24a3b968535f995a44f
rdw28 <rdw28@students.uwf.edu>
2019-09-30 17:48:50 -0400

    Describe friendly names for enumeration elements using Description annotation.  AB#62

--- AnthroCloud/AnthroCloud.Data/Indicator.cs
+++ AnthroCloud/AnthroCloud.Data/Indicator.cs
@@ -1,20 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel;
 
 namespace AnthroCloud.Data
 {
     public enum Indicator
     {
-        LengthForAge,
-        HeightForAge,
-        WeightForAge,
-        WeightForLength,
-        WeightForHeight,
-        BodyMassIndexForAge,
-        HeadCircumferenceForAge,
-        ArmCircumferenceForAge,
-        SubscapularSkinfoldForAge,
-        TricepsSkinfoldForAge
+        [Description("Length for Age")]
+        LFA,
+        [Description("Height for Age")]
+        HFA,
+        [Description("Weight for Age")]
+        WFA,
+        [Description("Weight for Length")]
+        WFL,
+        [Description("Weight for Height")]
+        WFH,
+        [Description("Body Mass Index for Age")]
+        BFA,
+        [Description("Head Circumference for Age")]
+        HCFA,
+        [Description("Arm Circumference for Age")]
+        ACFA,
+        [Description("Subscapular Skinfold for Age")]
+        SSF,
+        [Description("Triceps Skinfold for Age")]
+        TSF
     }
 }
