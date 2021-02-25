090a134a96b9c443419ff10bb82adfc531e5e00e
rdw100 <ryandustinwright@hotmail.com>
2021-02-24 19:04:18 -0500

    Added HTML elements for stats display.

--- AnthroCloud/AnthroCloud.UI.Blazor/Components/FormViewModel.cs
+++ AnthroCloud/AnthroCloud.UI.Blazor/Components/FormViewModel.cs
@@ -26,21 +26,21 @@ namespace AnthroCloud.UI.Blazor.Components
             SubscapularSkinFold = 7.00;
             WflPercentile = 61.4;
             WflZscore = 0.29;
-            //WfaPercentile = 51.9;
-            //WfaZscore = 0.05;
-            //LfaPercentile = 34.8;
-            //LfaZscore = -0.39;
-            //BfaPercentile = 64.1;
-            //BfaZscore = 0.36;
-            //HcaPercentile = 53.1;
-            //HcaZscore = 0.08;
-            //MuacPercentile = 74.3;
-            //MuacZscore = 0.65;
-            //TsfPercentile = 49.9;
-            //TsfZscore = 0.00;
-            //SsfPercentile = 65.0;
-            //SsfZscore = 0.38;
-            //Oedema = false;
+            WfaPercentile = 51.9;
+            WfaZscore = 0.05;
+            LfaPercentile = 34.8;
+            LfaZscore = -0.39;
+            BfaPercentile = 64.1;
+            BfaZscore = 0.36;
+            HcaPercentile = 53.1;
+            HcaZscore = 0.08;
+            MuacPercentile = 74.3;
+            MuacZscore = 0.65;
+            TsfPercentile = 49.9;
+            TsfZscore = 0.00;
+            SsfPercentile = 65.0;
+            SsfZscore = 0.38;
+            Oedema = false;
         }
 
         [Required]
@@ -66,38 +66,39 @@ namespace AnthroCloud.UI.Blazor.Components
 
         public Age Age { get; set; } //Todo: Move Age to Entities
 
-        public string AgeString { get; set;}
+        public string AgeString { get; set; } = "11mo";
 
         public byte AgeInMonths { get; set; } = 12;
 
         public byte AgeInYears { get; set; } = 0;
 
-        public double BMI { get; set; }
+        public double BMI { get; set; } = 16.9;
 
-        public double WfaPercentile { get; set; } //= 51.9;
-        public double WfaZscore { get; set; } //= 0.05;
+        public double WfaPercentile { get; set; } = 51.9;
+        public double WfaZscore { get; set; } = 0.05;
         public double HeadCircumference { get; set; } = 45.00;
         public double MUAC { get; set; } = 15.00;
         public double TricepsSkinFold { get; set; } = 8.00;
         public double SubscapularSkinFold { get; set; } = 7.00;
-        public double MuacPercentile { get; set; } //= 74.3;
-        public double MuacZscore { get; set; } //= 0.65;
-        public double WflPercentile { get; set; } //= 61.4;
-        public double WflZscore { get; set; } //= 0.29;
+        public double MuacPercentile { get; set; } = 74.3;
+        public double MuacZscore { get; set; } = 0.65;
+        public double WflPercentile { get; set; } = 61.4;
+        public double WflZscore { get; set; } = 0.29;
         public double WfhPercentile { get; set; }
         public double WfhZscore { get; set; }
-        public double TsfPercentile { get; set; } //= 49.9;
-        public double TsfZscore { get; set; } //= 0.00;
-        public double SsfPercentile { get; set; } //= 65.0;
-        public double SsfZscore { get; set; } //= 0.38;
-        public double LfaPercentile { get; set; } //= 34.8;
-        public double LfaZscore { get; set; } //= -0.39;
-        public double BfaPercentile { get; set; } //= 64.1;
-        public double BfaZscore { get; set; } //= 0.36;
-        public double HcaPercentile { get; set; } //= 53.1;
-        public double HcaZscore { get; set; } //= 0.08;
-        public double HfaPercentile { get; set; } //= 53.1;
-        public double HfaZscore { get; set; } //= 0.08;
+        public double TsfPercentile { get; set; } = 49.9;
+        public double TsfZscore { get; set; } = 0.00;
+        public double SsfPercentile { get; set; } = 65.0;
+        public double SsfZscore { get; set; } = 0.38;
+        public double LfaPercentile { get; set; } = 34.8;
+        public double LfaZscore { get; set; } = -0.39;
+        public double BfaPercentile { get; set; } = 64.1;
+        public double BfaZscore { get; set; } = 0.36;
+        public double HcaPercentile { get; set; } = 53.1;
+        public double HcaZscore { get; set; } = 0.08;
+        public double HfaPercentile { get; set; } = 53.1;
+        public double HfaZscore { get; set; } = 0.08;
+        public bool Oedema { get; set; } = false;
         //[Required]
         //[StringLength(10, ErrorMessage = "Name is too long.")]
         //public string Name { get; set; }
