83561bf18a81f351d96a257ed319baacc7c0fdad
rdw100 <ryandustinwright@hotmail.com>
2021-03-10 12:25:24 -0500

    Added modal popup in Blazor.  Use Blazored Modal for modal Weather Forecast data dialog.

--- AnthroCloud/AnthroCloud.UI.Blazor/Startup.cs
+++ AnthroCloud/AnthroCloud.UI.Blazor/Startup.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blazored.Modal;
 
 namespace AnthroCloud.UI.Blazor
 {
@@ -42,7 +43,7 @@ namespace AnthroCloud.UI.Blazor
             {
                 client.BaseAddress = new Uri(baseAddressPath);
             });
-
+            services.AddBlazoredModal();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
