9459a2135fc5bd968e48b8eddf85e6c1555f6aac
rdw100 <ryandustinwright@hotmail.com>
2021-03-12 20:04:05 -0500

    Added additional settings to send to Chart.  Anonymous type for vertical settings in C# presents issues for correlative javascript settings.

--- AnthroCloud/AnthroCloud.UI.Blazor/Components/LineChart.razor.cs
+++ AnthroCloud/AnthroCloud.UI.Blazor/Components/LineChart.razor.cs
@@ -64,19 +64,42 @@ namespace AnthroCloud.UI.Blazor.Components
                 Options = new
                 {
                     Responsive = true,
-                    //Title = new
-                    //{
-                    //    Display = true,
-                    //    Text = Title
-                    //},
+                    Title = new
+                    {
+                        Display = true,
+                        Text = "Birth to 5 Years (Z-scores)"
+                    },
                     Scales = new
                     {
-                        YAxes = new[]
+                        YAxes = new
                         {
-                        new { Ticks = new {
-                            BeginAtZero=true
-                        } }
-                    }
+                            Title = new
+                            {
+                                Display = true,
+                                Text = "Length (cm)"
+                            },
+                            ViewWindow = new
+                            { 
+                                Min = 80,
+                                Max = 110
+                            },
+                            Ticks = new[] 
+                            {
+                                50, 60, 70, 80, 90, 100, 110
+                            }
+                        },
+                        XAxes = new 
+                        {
+                            Title = new
+                            {
+                                Display = true,
+                                Text = "Weight (kg)"
+                            },
+                            Ticks = new[]
+                            {
+                                0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26
+                            }
+                        }
                     }
                 },
                 Data = new
