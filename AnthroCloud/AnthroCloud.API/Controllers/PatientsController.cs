de3e52ba54e948f4fab8d2bfffd6c2a41da2ee26
rdw100 <ryandustinwright@hotmail.com>
2022-09-25 10:03:46 -0400

    Added scaffolded web api controllers.

---
+++ AnthroCloud/AnthroCloud.API/Controllers/PatientsController.cs
@@ -0,0 +1,102 @@
+﻿using AnthroCloud.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnthroCloud.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientsController : ControllerBase
+    {
+        private readonly AssessmentContext _context;
+
+        public PatientsController(AssessmentContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Patients
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Patient>>> GetPatients()
+        {
+            return await _context.Patients.ToListAsync();
+        }
+
+        // GET: api/Patients/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Patient>> GetPatient(int id)
+        {
+            var patient = await _context.Patients.FindAsync(id);
+
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            return patient;
+        }
+
+        // PUT: api/Patients/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPatient(int id, Patient patient)
+        {
+            if (id != patient.PatientId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(patient).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PatientExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Patients
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Patient>> PostPatient(Patient patient)
+        {
+            _context.Patients.Add(patient);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPatient", new { id = patient.PatientId }, patient);
+        }
+
+        // DELETE: api/Patients/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePatient(int id)
+        {
+            var patient = await _context.Patients.FindAsync(id);
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            _context.Patients.Remove(patient);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PatientExists(int id)
+        {
+            return _context.Patients.Any(e => e.PatientId == id);
+        }
+    }
+}
---
+++ AnthroCloud/AnthroCloud.API/Controllers/VisitsController.cs
@@ -0,0 +1,102 @@
+﻿using AnthroCloud.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnthroCloud.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VisitsController : ControllerBase
+    {
+        private readonly AssessmentContext _context;
+
+        public VisitsController(AssessmentContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Visits
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Visit>>> GetVisits()
+        {
+            return await _context.Visits.ToListAsync();
+        }
+
+        // GET: api/Visits/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Visit>> GetVisit(int id)
+        {
+            var visit = await _context.Visits.FindAsync(id);
+
+            if (visit == null)
+            {
+                return NotFound();
+            }
+
+            return visit;
+        }
+
+        // PUT: api/Visits/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutVisit(int id, Visit visit)
+        {
+            if (id != visit.VisitId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(visit).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VisitExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Visits
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Visit>> PostVisit(Visit visit)
+        {
+            _context.Visits.Add(visit);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetVisit", new { id = visit.VisitId }, visit);
+        }
+
+        // DELETE: api/Visits/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVisit(int id)
+        {
+            var visit = await _context.Visits.FindAsync(id);
+            if (visit == null)
+            {
+                return NotFound();
+            }
+
+            _context.Visits.Remove(visit);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool VisitExists(int id)
+        {
+            return _context.Visits.Any(e => e.VisitId == id);
+        }
+    }
+}
