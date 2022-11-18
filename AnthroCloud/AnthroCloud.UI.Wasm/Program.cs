9d6e6ee8e82de81585afa3f6eabced50bae56d50
rdw100 <ryandustinwright@hotmail.com>
2022-11-18 07:14:42 -0500

    Added Treeview from Fluent UI Web Components for Blazor 1.6.  Formatted treeview column next to menu.  Icons appear in treeview.

--- AnthroCloud/AnthroCloud.UI.Wasm/Program.cs
+++ AnthroCloud/AnthroCloud.UI.Wasm/Program.cs
@@ -2,10 +2,13 @@ using AnthroCloud.UI.Wasm;
 using AnthroCloud.UI.Wasm.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Fast.Components.FluentUI;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
     builder.RootComponents.Add<App>("#app");
     builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddFluentUIComponents();
 
 string patientApiPath = builder.Configuration.GetValue<string>("ConfigurationSettings:patientApiPath");
 string visitApiPath = builder.Configuration.GetValue<string>("ConfigurationSettings:visitApiPath");
